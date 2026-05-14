3697d52e243154b0210bae61df55bfafa9344c0e
mtdes23 <thachminhtri23@gmail.com>
2026-05-14 08:27:45 +0700

    feat: add historical World Cup formations

--- src/stores/lineup.js
+++ src/stores/lineup.js
@@ -1,22 +1,26 @@
 import { defineStore } from 'pinia'
 import playersData from '../data/players.json'
+import { formations, getFormationData, getFormationKeys } from '../data/formations.js'
 
 export const useLineupStore = defineStore('lineup', {
   state: () => ({
     allPlayers: playersData,
     squad: Array(11).fill(null), // 11 slots for the lineup
-    formation: '4-3-3', // Default formation
+    formationKey: '4-3-3', // Default formation
   }),
   getters: {
     availablePlayers: (state) => {
       // Return players that are not currently placed in the squad
       const selectedIds = state.squad.map(p => p?.id).filter(Boolean)
       return state.allPlayers.filter(p => !selectedIds.includes(p.id))
-    }
+    },
+    currentFormation: (state) => getFormationData(state.formationKey),
+    allFormationKeys: () => getFormationKeys(),
+    allFormationsList: () => formations
   },
   actions: {
-    setFormation(newFormation) {
-      this.formation = newFormation
+    setFormation(newKey) {
+      this.formationKey = newKey
     },
     addPlayerToSquad(player, index) {
       this.squad[index] = player
