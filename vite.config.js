4893bb319f6a0d67e5afc59d90cce51b02cf1ce1
mtdes23 <thachminhtri23@gmail.com>
2026-05-14 11:03:55 +0700

    chore: update title to WorldCup History

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
       manifest: {
-        name: 'Worldcup History',
+        name: 'WorldCup History',
         short_name: 'WCHistory',
         description: 'Kho báu lịch sử bóng đá thế giới',
         theme_color: '#13072e',
